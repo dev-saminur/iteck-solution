9b077f79b2c92163a9d002acbef6b3a7b281fd9a
dev-saminur <saminur9125@gmail.com>
2024-03-27 02:05:11 +0600

    Update Heading.js

--- src/components/helpers/Heading/Heading.js
+++ src/components/helpers/Heading/Heading.js
@@ -3,7 +3,7 @@ import React from "react";
 const Heading = ({ className, title }) => {
   return (
     <h2
-      className={`text-[40px] text-[white] font-bold tracking-[20px] uppercase ${className}`}
+      className={`text-3xl lg:text-[40px] text-[white] font-bold tracking-[10px] lg:tracking-[10px] xl:tracking-[20px] uppercase ${className}`}
     >
       {title}
     </h2>
