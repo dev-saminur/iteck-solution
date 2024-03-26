8a4df32efc16cd4c2606159aecaf9c49c2b13853
dev-saminur <saminur9125@gmail.com>
2024-03-27 02:04:48 +0600

    Update Bottom.js

--- src/components/Chooseme/Bottom.js
+++ src/components/Chooseme/Bottom.js
@@ -6,34 +6,34 @@ import Peragraph from "../helpers/Peragraph/Peragraph";
 const Bottom = () => {
   return (
     <div className="mt-[100px]">
-      <div className="grid grid-cols-12 justify-items-center">
-        <div className="col-start-3 col-end-5 ">
+      <div className="grid grid-cols-12 gap-y-10 lg:gap-y-0 justify-items-start lg:justify-items-center">
+        <div className="col-start-4 col-end-9 lg:col-start-2 lg:col-end-6 xl:col-start-3 xl:col-end-5 ">
           <Flex className="items-center">
             <Heading
               title="25 +"
-              className="tracking-normal font-semibold mr-5"
+              className="lg:text-3xl xl:text-[40px] lg:tracking-[10px] xl:tracking-normal font-semibold mr-5"
             />
             <p className="text-[13px] text-[#CCCCCC] font-normal">
               YEARS <span className="block">OFEXPERIENCE</span>
             </p>
           </Flex>
         </div>
-        <div className="col-start-6 col-end-8">
+        <div className="col-start-4 col-end-9 lg:col-start-6 lg:col-end-9 xl:col-start-6 xl:col-end-8">
           <Flex className="items-center">
             <Heading
               title="1565"
-              className="tracking-normal font-semibold mr-5"
+              className="lg:text-3xl xl:text-[40px] lg:tracking-[10px] xl:tracking-normal font-semibold mr-5"
             />
             <p className="text-[13px] text-[#CCCCCC] font-normal">
               PROJECTS <span className="block">COMPLETED</span>
             </p>
           </Flex>
         </div>
-        <div className="col-start-9 col-end-11 ">
+        <div className="col-start-4 col-end-9 lg:col-start-9 lg:col-end-12 xl:col-start-9 xl:col-end-11 ">
           <Flex className="items-center">
             <Heading
               title="240"
-              className="tracking-normal font-semibold mr-5"
+              className="lg:text-3xl xl:text-[40px] lg:tracking-[10px] xl:tracking-normal font-semibold mr-5"
             />
             <p className="text-[13px] text-[#CCCCCC] font-normal">
               SATISFIED CLIENTS ON
