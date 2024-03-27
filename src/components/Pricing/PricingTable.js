a2560a05ca03928a541adfacb3f37c97917e8909
dev-saminur <saminur9125@gmail.com>
2024-03-27 11:42:14 +0600

    Update PricingTable.js

--- src/components/Pricing/PricingTable.js
+++ src/components/Pricing/PricingTable.js
@@ -18,15 +18,6 @@ const PricingTable = () => {
         <div className="text-center flex items-center justify-center mb-5">
           <p className="text-sm text-white">Billed monthly</p>
           <div onClick={() => setShow(!show)} className="mx-2">
-            {/* <div className="dark:bg-black/10">
-              <label className="text-white">
-                <input
-                  className="dark:border-white-400/20 dark:scale-100 transition-all duration-500 ease-in-out dark:hover:scale-110 dark:checked:scale-100 w-10 h-10"
-                  type="checkbox"
-                />
-              </label>
-            </div> */}
-
             {show ? (
               <div className="w-[40px] h-[19px] relative bg-[#4096FF] border border-solid rounded-3xl  border-[#15F1FF] after:content-[' ']  after:absolute after:top-[50%] after:translate-y-[-50%] after:right-0 after:w-[16px] after:h-[16px] after:rounded-full after:bg-white  "></div>
             ) : (
@@ -36,8 +27,8 @@ const PricingTable = () => {
           <p className="text-sm text-white">Billed annually</p>
         </div>
         {show ? (
-          <Flex className="justify-between ">
-            <div className="w-[315px] border border-solid border-[#3D9DD1]  py-[85px] px-[50px] text-center">
+          <Flex className="flex-wrap gap-y-8 lg-gap-y-0 justify-center lg:justify-between">
+            <div className="max-w-[315px] lg:w-[315px] border border-solid border-[#3D9DD1]  py-[85px] px-[50px] text-center">
               <div className="flex justify-center">
                 <Image src={priceOne} alt="pricing" />
               </div>
@@ -64,7 +55,7 @@ const PricingTable = () => {
                 Ger Started Now
               </Link>
             </div>
-            <div className="w-[315px] border-x-0 box-border border-t-[12px] border-t-[#15F1FF] border-b border-solid border-[#3D9DD1]  pt-[15px] pb-[85px] px-[50px] text-center">
+            <div className="max-w-[315px] lg:w-[315px]    lg:border-x-0  border-t-[12px] border-t-[#15F1FF] border border-solid border-[#3D9DD1]  pt-[15px] pb-[85px] px-[50px] text-center">
               <button className="bg-[#1f3f85] py-[5px] px-2 text-white mb-9 text-xs">
                 Recommended
               </button>
@@ -94,7 +85,7 @@ const PricingTable = () => {
                 Ger Started Now
               </Link>
             </div>
-            <div className="w-[315px] border border-solid border-[#3D9DD1]  py-[85px] px-[50px] text-center">
+            <div className="max-w-[315px] lg:w-[315px] border border-solid border-[#3D9DD1]  py-[85px] px-[50px] text-center">
               <div className="flex justify-center">
                 <Image src={priceOne} alt="pricing" />
               </div>
@@ -123,8 +114,8 @@ const PricingTable = () => {
             </div>
           </Flex>
         ) : (
-          <Flex className="justify-between ">
-            <div className="w-[315px] border border-solid border-[#3D9DD1]  py-[85px] px-[50px] text-center">
+          <Flex className="flex-wrap gap-y-8 lg-gap-y-0 justify-center lg:justify-between ">
+            <div className="max-w-[315px] lg:w-[315px] border border-solid border-[#3D9DD1]  py-[85px] px-[50px] text-center">
               <div className="flex justify-center">
                 <Image src={priceOne} alt="pricing" />
               </div>
@@ -151,7 +142,7 @@ const PricingTable = () => {
                 Ger Started Now
               </Link>
             </div>
-            <div className="w-[315px] border-x-0 box-border border-t-[12px] border-t-[#15F1FF] border-b border-solid border-[#3D9DD1]  pt-[15px] pb-[85px] px-[50px] text-center">
+            <div className="max-w-[315px] lg:w-[315px]    lg:border-x-0  border-t-[12px] border-t-[#15F1FF] border border-solid border-[#3D9DD1]  pt-[15px] pb-[85px] px-[50px] text-center">
               <button className="bg-[#1f3f85] py-[5px] px-2 text-white mb-9 text-xs">
                 Recommended
               </button>
@@ -181,7 +172,7 @@ const PricingTable = () => {
                 Ger Started Now
               </Link>
             </div>
-            <div className="w-[315px] border border-solid border-[#3D9DD1]  py-[85px] px-[50px] text-center">
+            <div className="max-w-[315px] lg:w-[315px] border border-solid border-[#3D9DD1]  py-[85px] px-[50px] text-center">
               <div className="flex justify-center">
                 <Image src={priceOne} alt="pricing" />
               </div>
@@ -210,92 +201,6 @@ const PricingTable = () => {
             </div>
           </Flex>
         )}
-        {/* <Flex className="justify-between ">
-          <div className="w-[315px] border border-solid border-[#3D9DD1]  py-[85px] px-[50px] text-center">
-            <div className="flex justify-center">
-              <Image src={priceOne} alt="pricing" />
-            </div>
-            <p className="text-base text-white mt-[30px] mb-4 font-bold">
-              Personal
-            </p>
-            <h4 className="text-[32px] text-white mb-2">
-              <span className="text-[13px] ">
-                <sup className="top-[-16px]">$</sup>
-              </span>
-              20.9
-              <span className="text-[13px]"> / month</span>
-            </h4>
-            <p className="text-base text-[#A7BCD0] py-4  uppercase mb-4 font-normal border-y border-solid border-[#A7BCD0]">
-              3 PROJECTS
-            </p>
-            <p className="text-base text-[#A7BCD0]   uppercase mb-4 font-normal ">
-              6 MONTHS SUPPORT
-            </p>
-            <Link
-              href="#"
-              className="text-xs mt-[50px] text-white block  font-semibold  hover:text-[#05437B] py-[10px] px-6 border rounded-full border-[#15F1FF] hover:border-[#15F1FF] hover:bg-[#15F1FF] transition-all ease-in after:duration-300"
-            >
-              Ger Started Now
-            </Link>
-          </div>
-          <div className="w-[315px] border-x-0 box-border border-t-[12px] border-t-[#15F1FF] border-b border-solid border-[#3D9DD1]  pt-[15px] pb-[85px] px-[50px] text-center">
-            <button className="bg-[#1f3f85] py-[5px] px-2 text-white mb-9 text-xs">
-              Recommended
-            </button>
-            <div className="flex justify-center">
-              <Image src={priceOne} alt="pricing" />
-            </div>
-            <p className="text-base text-white mt-[30px] mb-4 font-bold">
-              Personal
-            </p>
-            <h4 className="text-[32px] text-white mb-2">
-              <span className="text-[13px] ">
-                <sup className="top-[-16px]">$</sup>
-              </span>
-              20.9
-              <span className="text-[13px]"> / month</span>
-            </h4>
-            <p className="text-base text-[#A7BCD0] py-4  uppercase mb-4 font-normal border-y border-solid border-[#A7BCD0]">
-              3 PROJECTS
-            </p>
-            <p className="text-base text-[#A7BCD0]   uppercase mb-4 font-normal ">
-              6 MONTHS SUPPORT
-            </p>
-            <Link
-              href="#"
-              className="text-xs mt-[50px] text-white block  font-semibold  hover:text-[#05437B] py-[10px] px-6 border rounded-full border-[#15F1FF] hover:border-[#15F1FF] hover:bg-[#15F1FF] transition-all ease-in after:duration-300"
-            >
-              Ger Started Now
-            </Link>
-          </div>
-          <div className="w-[315px] border border-solid border-[#3D9DD1]  py-[85px] px-[50px] text-center">
-            <div className="flex justify-center">
-              <Image src={priceOne} alt="pricing" />
-            </div>
-            <p className="text-base text-white mt-[30px] mb-4 font-bold">
-              Personal
-            </p>
-            <h4 className="text-[32px] text-white mb-2">
-              <span className="text-[13px] ">
-                <sup className="top-[-16px]">$</sup>
-              </span>
-              20.9
-              <span className="text-[13px]"> / month</span>
-            </h4>
-            <p className="text-base text-[#A7BCD0] py-4  uppercase mb-4 font-normal border-y border-solid border-[#A7BCD0]">
-              3 PROJECTS
-            </p>
-            <p className="text-base text-[#A7BCD0]   uppercase mb-4 font-normal ">
-              6 MONTHS SUPPORT
-            </p>
-            <Link
-              href="#"
-              className="text-xs mt-[50px] text-white block  font-semibold  hover:text-[#05437B] py-[10px] px-6 border rounded-full border-[#15F1FF] hover:border-[#15F1FF] hover:bg-[#15F1FF] transition-all ease-in after:duration-300"
-            >
-              Ger Started Now
-            </Link>
-          </div>
-        </Flex> */}
       </div>
     </div>
   );
