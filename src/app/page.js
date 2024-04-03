8637c8cacc3fa39644039154078e8a493504afe4
dev-saminur <saminur9125@gmail.com>
2024-04-03 19:40:04 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-// import Chooseme from "@/components/Chooseme";
-// import Contact from "@/components/Contact";
-// import Editiorial from "@/components/Editiorial";
-// import Hero from "@/components/Hero/Hero";
-// import Pricing from "@/components/Pricing";
-// import Review from "@/components/Review";
-// import Service from "@/components/Service";
+import Chooseme from "@/components/Chooseme";
+import Contact from "@/components/Contact";
+import Editiorial from "@/components/Editiorial";
+import Hero from "@/components/Hero/Hero";
+import Pricing from "@/components/Pricing";
+import Review from "@/components/Review";
+import Service from "@/components/Service";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
@@ -12,13 +12,13 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      {/* <Hero />
+      <Hero />
       <Service />
       <Chooseme />
       <Review />
       <Pricing />
       <Editiorial />
-      <Contact /> */}
+      <Contact />
       <Footer />
     </>
   );
