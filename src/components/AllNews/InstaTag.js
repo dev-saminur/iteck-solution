aec443de80f196cfb64cb1b335562f7c8ec24008
dev-saminur <saminur9125@gmail.com>
2024-04-07 09:16:14 +0600

    Update InstaTag.js

--- src/components/AllNews/InstaTag.js
+++ src/components/AllNews/InstaTag.js
@@ -9,6 +9,7 @@ import NewsBlog5 from "../../../public/assets/blogs/news5.png";
 import NewsBlog6 from "../../../public/assets/blogs/news6.png";
 import Image from "next/image";
 import { IoLogoInstagram } from "react-icons/io";
+import { InstaData } from "./data";
 
 const InstaTag = () => {
   return (
@@ -19,40 +20,23 @@ const InstaTag = () => {
             OUR INSTAGRAM
           </h3>
 
-          <Flex className="justify-between ">
-            <div className="relative w-[120px] h-[100px] group rounded-[10px] overflow-hidden bg-gray-700">
-              <Image
-                width={120}
-                height={100}
-                src={NewsBlog}
-                alt="our-instagram"
-                className="w-full h-full bg-cover object-cover bg-center rounded-[10px]"
-              />
-              <div className="bg-black w-full h-full absolute top-0 left-0 opacity-0 group-hover:opacity-50 duration-200"></div>
-              <IoLogoInstagram className="text-white absolute top-[45%] left-[42%] z-10 translate-[-50%] duration-200 opacity-0 group-hover:opacity-100" />
-            </div>
-            <div className="relative w-[120px] h-[100px] group rounded-[10px] overflow-hidden bg-gray-700">
-              <Image
-                width={120}
-                height={100}
-                src={NewsBlog}
-                alt="our-instagram"
-                className="w-full h-full bg-cover object-cover bg-center rounded-[10px]"
-              />
-              <div className="bg-black w-full h-full absolute top-0 left-0 opacity-0 group-hover:opacity-50 duration-200"></div>
-              <IoLogoInstagram className="text-white absolute top-[45%] left-[42%] z-10 translate-[-50%] duration-200 opacity-0 group-hover:opacity-100" />
-            </div>
-            <div className="relative w-[120px] h-[100px] group rounded-[10px] overflow-hidden bg-gray-700">
-              <Image
-                width={120}
-                height={100}
-                src={NewsBlog}
-                alt="our-instagram"
-                className="w-full h-full bg-cover object-cover bg-center rounded-[10px]"
-              />
-              <div className="bg-black w-full h-full absolute top-0 left-0 opacity-0 group-hover:opacity-50 duration-200"></div>
-              <IoLogoInstagram className="text-white absolute top-[45%] left-[42%] z-10 translate-[-50%] duration-200 opacity-0 group-hover:opacity-100" />
-            </div>
+          <Flex className="justify-between flex-wrap">
+            {InstaData.map((item, i) => (
+              <div
+                key={i}
+                className="relative w-[120px] h-[100px] group rounded-[10px] overflow-hidden bg-gray-700 mb-3"
+              >
+                <Image
+                  width={120}
+                  height={100}
+                  src={item.imgages}
+                  alt="our-instagram"
+                  className="w-full h-full bg-cover object-cover bg-center rounded-[10px]"
+                />
+                <div className="bg-black w-full h-full absolute top-0 left-0 opacity-0 group-hover:opacity-50 duration-200"></div>
+                <IoLogoInstagram className="text-white absolute top-[45%] left-[42%] z-10 translate-[-50%] duration-200 opacity-0 group-hover:opacity-100" />
+              </div>
+            ))}
           </Flex>
         </div>
         <div className="mt-[50px]">
