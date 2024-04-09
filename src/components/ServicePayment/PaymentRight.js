d0fa14f174fa97403c5a26dae665bba390a23362
dev-saminur <saminur9125@gmail.com>
2024-04-09 23:42:01 +0600

    Update PaymentRight.js

--- src/components/ServicePayment/PaymentRight.js
+++ src/components/ServicePayment/PaymentRight.js
@@ -12,14 +12,14 @@ const PaymentRight = () => {
           width={750}
           height={553}
           alt="payment"
-          className="absolute top-0 right-0 z-10 animate-spin animate-duration-[20000ms] animate-ease-linear "
+          className="absolute w-[68%] lg:w-[85%] top-0 right-[22px] sm:right-[74px] lg:right-0 z-10  animate-spin animate-duration-[20000ms] animate-ease-linear "
         />
         <Image
           src={PaymentImgTwo}
           width={749}
           height={553}
           alt="payment"
-          className="absolute top-0 left-[170px] z-20 animate-[topbottom] animate-infinite animate-duration-[6000ms] animate-delay-1000 animate-ease-in-out animate-alternate animate-fill-both"
+          className="absolute scale-[.6] lg:scale-100 top-0 left-0 xxl:left-[170px] z-20 animate-[topbottom] animate-infinite animate-duration-[6000ms] animate-delay-1000 animate-ease-in-out animate-alternate animate-fill-both"
         />
       </div>
     </>
