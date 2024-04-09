0b58ac158c46e3eb0fecc634ce47a4bcb201d36a
dev-saminur <saminur9125@gmail.com>
2024-04-09 12:52:02 +0600

    Update index.js

--- src/components/AboutContact/index.js
+++ src/components/AboutContact/index.js
@@ -5,10 +5,10 @@ import ContactForm from "./ContactForm";
 const AboutContact = () => {
   return (
     <>
-      <div className="py-[100px] bg-[#FFFFFF]">
+      <div className="py-[100px] bg-[#FFFFFF] lg:px-0 px-4">
         <Container>
           <div className="mb-[70px] text-center">
-            <h2 className="text-[45px] font-bold text-[#212529] leading-[1.4] mb-5 z-50">
+            <h2 className="text-[30px] lg:text-[45px] font-bold text-[#212529] leading-[1.4] mb-5 z-50">
               Ready To Start A
               <span className="text-[#157EFB] ml-2  relative after:absolute after:content-[''] after:bottom-0 after:left-0 after:w-[100%] after:h-[40%] after:bg-[#157EFB] after:opacity-[.1]">
                 Projects
