1d5301a7e670aaaa7799d5c3be8cb0f13732a11b
dev-saminur <saminur9125@gmail.com>
2024-04-09 15:15:16 +0600

    Update index.js

--- src/components/OurServices/index.js
+++ src/components/OurServices/index.js
@@ -5,10 +5,10 @@ import ServiceCard from "./ServiceCard";
 const OurServices = () => {
   return (
     <>
-      <div className="bg-[#E7F1FF] py-[100px]">
+      <div className="bg-[#E7F1FF] py-[100px] sm:px-4 lg:px-0">
         <Container>
           <div className="mb-[70px] text-center">
-            <h2 className="text-[45px] font-bold text-[#212529] leading-[1.4] mb-5 z-50">
+            <h2 className="text-[30px] lg:text-[45px] font-bold text-[#212529] leading-[1.4] mb-5 z-50">
               Our
               <span className="text-[#157EFB] ml-2  relative after:absolute after:content-[''] after:bottom-0 after:left-0 after:w-[100%] after:h-[40%] after:bg-[#157EFB] after:opacity-[.1]">
                 Services
