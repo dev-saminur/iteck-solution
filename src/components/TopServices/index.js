9846a50012acd5278b3246d6dc0f6dbc888a80c4
dev-saminur <saminur9125@gmail.com>
2024-04-09 15:15:41 +0600

    Update index.js

--- src/components/TopServices/index.js
+++ src/components/TopServices/index.js
@@ -8,13 +8,13 @@ const TopServices = () => {
       <div className="py-[100px]">
         <Container>
           <div className="mb-[70px] text-center">
-            <h2 className="text-[45px] font-bold text-[#212529] leading-[1.4] mb-5 z-50">
+            <h2 className="text-[30px] lg:text-[45px] font-bold text-[#212529] leading-[1.4] mb-5 z-50">
               Our Top
               <span className="text-[#157EFB] ml-2  relative after:absolute after:content-[''] after:bottom-0 after:left-0 after:w-[100%] after:h-[40%] after:bg-[#157EFB] after:opacity-[.1]">
                 Services
               </span>
             </h2>
-            <p className="text-[15px] text-[#666666] leading-[1.7]  text-center px-[350px]">
+            <p className="text-[15px] text-[#666666] leading-[1.7]  text-center lg:px-[350px]">
               Iteck Dashboard is a powerhouse when it comes to the feature list.
               This ensures you have every functionality you need to build, run,
               and expand your marketplace
