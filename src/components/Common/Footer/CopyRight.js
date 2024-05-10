df32501272892c1bea88b979d81d627418155c6f
dev-saminur <saminur9125@gmail.com>
2024-05-10 22:16:33 +0600

    Update CopyRight.js

--- src/components/Common/Footer/CopyRight.js
+++ src/components/Common/Footer/CopyRight.js
@@ -11,7 +11,12 @@ const CopyRight = () => {
         <div className="pt-[30px] text-start  lg:text-center border-t border-solid border-[#45447A]">
           <Container>
             <Flex className="flex-wrap lg:flex items-center">
-              <Image src={FooterLogo} width={100} height={100} />
+              <Image
+                src={FooterLogo}
+                width={100}
+                height={100}
+                alt="footer-logo"
+              />
               <span className="text-[13px] text-[#8D8DAE] font-normal ml-0 mt-8 lg:mt-0  lg:ml-[170px] ">
                 © 2023 Copyrights by
                 <Link
