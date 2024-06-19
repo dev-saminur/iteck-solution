f9bb94d23d9a657de25460bf261fae4d9829e4fc
dev-saminur <saminur9125@gmail.com>
2024-06-19 11:20:40 +0600

    Update FooterRight.js

--- src/components/Footer/FooterRight.js
+++ src/components/Footer/FooterRight.js
@@ -4,7 +4,7 @@ import Flex from "../helpers/Flex/Flex";
 
 const FooterRight = () => {
   return (
-    <Flex className="items-center gap-x-6 ">
+    <Flex className="items-center gap-x-6  mt-0 lg:mt-5 xl:mt-0">
       <div className="hover:bg-[#15f1ff] w-[40px] h-[40px] rounded-full flex justify-center items-center border border-solid border-[#15f1ff] ease-in transition-all duration-300">
         <FaTwitter color="#FFFFFF" />
       </div>
